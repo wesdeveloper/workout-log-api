136ee512bbab00ba3aaa211d4b355613664c140a
wesdeveloper <wesleyalopes2010@gmail.com>
2021-03-18 21:34:52 -0300

    chore(tests): update tests setup

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,13 +1,13 @@
 const { resolve } = require('path');
 
-const root = resolve(__dirname);
+const root = resolve(__dirname, '..');
 // eslint-disable-next-line import/no-dynamic-require
-const rootConfig = require(`${root}/jest-config.js`);
+const rootConfig = require(`${root}/jest.config.js`);
 
 module.exports = {
   ...rootConfig,
   rootDir: root,
-  displayName: 'end2end-tests',
+  displayName: 'functional-tests',
   setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
   testMatch: ['<rootDir>/test/**/*.test.ts'],
 };
